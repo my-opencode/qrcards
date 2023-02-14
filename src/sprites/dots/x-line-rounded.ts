eaca66208929d6a2ab4f11ece6805b3441aa749f
lanterieur <35257468+lanterieur@users.noreply.github.com>
2023-02-15 03:54:55 +0800

    add sprites to svg gen wip fixed sprites #80

--- src/sprites/dots/x-line-rounded.ts
+++ src/sprites/dots/x-line-rounded.ts
@@ -1,19 +1,10 @@
 import { DotSprites } from "../DotSprites";
 
-const definitions = `<path id="1-rb-" d="M0,100h100V0C44.9,0,0,44.9,0,100z$/>
-  <rect id="threeFourTbLr" x="0" y="0"  width="100" height="100"/>
-  <path id="1--bl" d="M0,0v100h100C100,44.9,55.1,0,0,0z$/>
-  <path id="1--b-" d="M100,50c0,11.7,0,50,0,50s-34,0-50,0c-12.6,0-50,0-50,0s0-34.9,0-50C0,22.3,22.3,0,50,0
-    S100,22.3,100,50z$/>
-  <path id="1t--l" d="M100,0H0v100C55.1,100,100,55.1,100,0z$/>
-  <path id="1-r--" d="M50,0c11.7,0,50,0,50,0s0,34,0,50c0,12.6,0,50,0,50s-34.9,0-50,0C22.3,100,0,77.7,0,50
-    S22.3,0,50,0z$/>
-  <path id="1tr--" d="M100,100V0H0C0,55.1,44.9,100,100,100z$/>
-  <path id="1t---" d="M0,50C0,38.3,0,0,0,0s34,0,50,0c12.6,0,50,0,50,0s0,34.9,0,50c0,27.7-22.3,50-50,50S0,77.7,0,50
-    z$/>
-  <path id="1----" d="M50,0c27.7,0,50,22.3,50,50s-22.3,50-50,50S0,77.7,0,50S22.3,0,50,0z$/>
-  <path id="1---l" d="M50,100c-11.7,0-50,0-50,0s0-34,0-50C0,37.4,0,0,0,0s34.9,0,50,0c27.7,0,50,22.3,50,50
-    S77.7,100,50,100z$/>`;
+const definitions = `
+  <path id="threeFourLr" d="M0,7.7H100V92.3H0V7.7z"/>
+  <path id="1-r--" d="M42.3,7.7H100v84.6H42.3C18.9,92.3,0,73.4,0,50S18.9,7.7,42.3,7.7z"/>
+  <path id="1---l" d="M57.7,7.7H0v84.6H57.7C81.1,92.3,100,73.4,100,50S81.1,7.7,57.7,7.7z"/>
+  <path id="1----" d="M50,0c27.7,0,50,22.3,50,50s-22.3,50-50,50S0,77.7,0,50S22.3,0,50,0z"/>`;
 const useIds = {
   // "0----": " ",
   // "0t---": " ",
@@ -32,20 +23,20 @@ const useIds = {
   // "0-rbl": " ",
   // "0trbl": " ",
   "1----": "1----",
-  "1t---": "1t---",
+  "1t---": "1----",
   "1-r--": "1-r--",
-  "1--b-": "1--b-",
+  "1--b-": "1----",
   "1---l": "1---l",
-  "1tr--": "1tr--",
-  "1t-b-": "threeFourTbLr",
-  "1t--l": "1t--l",
-  "1-rb-": "1-rb-",
-  "1-r-l": "threeFourTbLr",
-  "1--bl": "1--bl",
-  "1trb-": "threeFourTbLr",
-  "1t-bl": "threeFourTbLr",
-  "1tr-l": "threeFourTbLr",
-  "1-rbl": "threeFourTbLr",
-  "1trbl": "threeFourTbLr",
+  "1tr--": "1-r--",
+  "1t-b-": "1----",
+  "1t--l": "1---l",
+  "1-rb-": "1-r--",
+  "1-r-l": "threeFourLr",
+  "1--bl": "1---l",
+  "1trb-": "1-r--",
+  "1t-bl": "1---l",
+  "1tr-l": "threeFourLr",
+  "1-rbl": "threeFourLr",
+  "1trbl": "threeFourLr",
 };
 export const sprites = new DotSprites(`Rounded horizontal lines`,definitions, useIds);
